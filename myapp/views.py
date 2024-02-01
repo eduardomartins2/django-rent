4d675ed45938aa5e11bd7f74132f4df505673c4c
Eduardo Martins <eduardo.amaral382@gmail.com>
2024-02-01 20:35:44 -0300

    update

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Immobile
+from django.shortcuts import render, redirect
+from myapp.forms import ClientForm, ImmobileForm
+from .models import Immobile, ImmobileImage
 
 # Create your views here.
 def list_location(request):
@@ -8,3 +9,30 @@ def list_location(request):
         'immobiles': immobiles
     }
 	return render(request, 'list-location.html', context)
+
+
+def form_client(request):
+    form = ClientForm() 
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list-location')   
+    return render(request, 'form-client.html', {'form': form})
+
+
+def form_immobile(request):
+    form = ImmobileForm()
+    if request.method == 'POST':
+        form = ImmobileForm(request.POST, request.FILES)
+        if form.is_valid():
+            immobile = form.save()
+            files = request.FILES.getlist('immobile') ## pega todas as imagens
+            if files:
+                for f in files:
+                    ImmobileImage.objects.create( # cria instance para imagens
+                        immobile=immobile, 
+                        image=f)
+            return redirect('list-location')  
+    return render(request, 'form-immobile.html', {'form': form})
+
